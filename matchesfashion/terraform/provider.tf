f5109b66d0ca08ff387c992b0a1a31776718a48c
rahul-gupta-151087 <rahul.gupta151087@gmail.com>
2019-05-20 17:53:48 +0100

    Delete provider.tf

--- matchesfashion/terraform/provider.tf
+++
@@ -1,8 +0,0 @@
-#======== Defining provider details for AWS ========
-
- provider "aws" {
-  region  = "eu-west-2"
-  version = "~> 1.60.0"
-}
-
-#===================================================
