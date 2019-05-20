d1348e81b2296f7f585f4e65061da1ac7ca49c4f
Rahul Gupta <rahul.gupta@origamienergy.com>
2019-05-20 17:45:05 +0100

    New directory

---
+++ matchesfashion/terraform/provider.tf
@@ -0,0 +1,8 @@
+#======== Defining provider details for AWS ========
+
+ provider "aws" {
+  region  = "eu-west-2"
+  version = "~> 1.60.0"
+}
+
+#===================================================
